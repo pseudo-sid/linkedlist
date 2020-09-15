983960c15d925eac00d5126d155afe80f8e28198
Siddharth Singh <siddharth19111@iiitd.ac.in>
2020-09-15 23:07:39 +0530

    methods of list class

--- stl/stllist.cpp
+++ stl/stllist.cpp
@@ -1,9 +1,17 @@
 #include<iostream>
 #include<list>
+#include<string>
 
 using namespace std;
 int main(){
-    list<int> l1{1,2,3,10,8,5};
-    
-
+    list<int> l1;
+    l1.push_front(1);
+    list<string> l2({"apple","guava", "mango", "banana"});
+    l2.push_back("pineapple");
+    cout << l2.front()<<endl;
+    l2.pop_front();
+    for(auto s: l2)
+        cout << s <<"->";
+    cout << endl;
+    return 0;
 }
